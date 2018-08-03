8e5463e21036d85bd13aa80095e76bfa7e796719
artdem <artdemrev@gmail.com>
2018-08-03 09:52:19 +0200

    Dodana funkcja usuwania pracowników za pomocą numeru ID.

--- src/com/company/Removal.java
+++ src/com/company/Removal.java
@@ -5,18 +5,15 @@ import java.util.UUID;
 
 public class Removal {
 
-    public static void employeeRemoval(){
+    public static void employeeRemoval() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj ID pracownika do usunięcia:");
         String idRemoval = scanner.nextLine();
-        for (Employee i : Database.ALL_EMPLOYEES)
-            if (i.getUuid().equals(idRemoval)){
+        for (int i = 0; i < Database.ALL_EMPLOYEES.size(); i++) {
+            if (Database.ALL_EMPLOYEES.get(i).getUuid().toString().equals(idRemoval)) {
+                System.out.println(Database.ALL_EMPLOYEES.get(i) + " - Pracownik usunięty.");
                 Database.ALL_EMPLOYEES.remove(i);
-                System.out.println(i + " - Pracownik usunięty.");
-                AppMenu.mainMenu();
-            } else {
-                System.out.println("Pracownik o podanym ID nie istnieje.");
-                AppMenu.mainMenu();
             }
+        }
     }
 }
